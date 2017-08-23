5276501c842a27672f1e7d4ea5ab636541a6d831
Edmund Martin <edmartin101@googlemail.com>
2017-08-23 22:12:06 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"encoding/csv"
 	"log"
 	"os"
 	"path/filepath"
@@ -63,16 +63,58 @@ func checkip(googlehits []string) []string {
 	return validatedhits
 }
 
+func fixDate(date string) (string, string) {
+	date = strings.Replace(date, "[", "", 1)
+	dateTime := strings.Split(date, ":")
+	date = dateTime[0]
+	time := strings.Join(dateTime[1:], ":")
+	return date, time
+}
+
+func formatOutput(line string) []string {
+	splitLine := strings.Split(line, " ")
+	ipAddress := splitLine[0]
+	date, time := fixDate(splitLine[3])
+	url := splitLine[6]
+	status := splitLine[8]
+	numbytes := splitLine[9]
+	useragent := strings.Join(splitLine[11:], " ")
+	csvRow := []string{}
+	csvRow = append(csvRow, ipAddress, date, time, url, status, numbytes, useragent)
+	return csvRow
+}
+
+func resultsToCsv(verifiedhits []string) {
+	records := [][]string{}
+
+	for _, f := range verifiedhits {
+		result := formatOutput(f)
+		records = append(records, result)
+	}
+
+	file, err := os.Create("logs.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, value := range records {
+		err := writer.Write(value)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
+
 func main() {
 	list := getLogs("*log")
-	fmt.Print(list)
 
 	for _, f := range list {
 		res := checkuseragent(f)
-		fmt.Print(res)
 		res2 := checkip(res)
-		for _, f := range res2 {
-			fmt.Println(f)
-		}
+		resultsToCsv(res2)
 	}
 }
